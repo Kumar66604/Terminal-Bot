8a1cd26a808d3d3d984104fbea1c4b8e8a235b8e
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-04-11 14:25:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import re
 id_pattern = re.compile(r'^.\d+$')
 
 
-token = os.environ.get("TOKEN")
-app_id = int(os.environ.get("APP_ID"))
-app_hash = os.environ.get("API_HASH")
-allowed = [int(user) if id_pattern.search(user) else user for user in os.environ.get('AUTH_USERS', '').split()]
+token = os.environ.get("TOKEN","7176123540:AAFtY7Vepb1_0j4Z835LdOr9uomHvNT_C_o")
+app_id = int(os.environ.get("APP_ID", 15657755))
+app_hash = os.environ.get("API_HASH", "7cce51d4664d010b90ad690e0d5121ad")
+allowed = [int(user) if id_pattern.search(user) else user for user in os.environ.get('AUTH_USERS', '5003133371').split()]
 
 help_text = """
 Hello I'm Terminal Bot which will Execute your Commands.
